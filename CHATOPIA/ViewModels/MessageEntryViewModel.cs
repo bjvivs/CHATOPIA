f86ba3706f4166621fbfbceaf1bde35d341a07c4
Richard Bennett <richard@tailoredpos.com>
2014-06-15 00:47:57 -0400

    screens are basically wired up.

--- CHATOPIA/ViewModels/MessageEntryViewModel.cs
+++ CHATOPIA/ViewModels/MessageEntryViewModel.cs
@@ -1,43 +1,36 @@
-﻿using Caliburn.Micro;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CHATOPIA.ViewModels {
+	using Caliburn.Micro;
+	using System;
+	class MessageEntryViewModel : Screen {
+		// member variable
+		private string _Message;
+		IEventAggregator _EventAggregator;
 
-namespace CHATOPIA.ViewModels
-{
-    class MessageEntryViewModel : Screen
-    {
-        // member variable
-        private string _Message;
-        IEventAggregator _EventAggregator;
+		public MessageEntryViewModel(IEventAggregator eventAggregator) {
+			_EventAggregator = eventAggregator;
+		}
 
-        /// <summary>
-        /// contains the text to be sent 
-        /// </summary>
-        /// property becaues it doesn't have a parameter list (not a function) and it can return a value
-        public string Message
-        {
-            get { return _Message; }
-            set 
-            {
-                _Message = value;
-                NotifyOfPropertyChange(() => Message);
-            }
-        }
-        // method
-        // TODO: fix bug
-        public void Send() {
+		/// <summary>
+		/// contains the text to be sent 
+		/// </summary>
+		/// property becaues it doesn't have a parameter list (not a function) and it can return a value
+		public string Message {
+			get { return _Message; }
+			set {
+				_Message = value;
+				NotifyOfPropertyChange(() => Message);
+			}
+		}
+		// method
+		// TODO: fix bug
+		public void Send() {
 
-            if (_Message != string.Empty)
-            { 
-                _EventAggregator.PublishOnUIThread(new AggEvents.SendMessageAggEvent { Message = this.Message }); 
-            }
-            
-            _Message = String.Empty;
-            
+			if (_Message != string.Empty) {
+				_EventAggregator.PublishOnUIThread(new AggEvents.SendMessageAggEvent { Message = this.Message });
+			}
 
-        }
-    }
+			_Message = String.Empty;
+
+		}
+	}
 }
